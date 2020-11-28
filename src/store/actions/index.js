4c5aadee2846fc7c3cfce63607aeaac8c47ead6b
tran tan tai <trantantai20081997@gmail.com>
2020-11-28 13:13:09 +0700

    add Auth firebase

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,8 +3,19 @@ export {
     removeIngredient,
     initIngredients
 } from './burgerBuilder';
+
 export {
     purchaseBurger,
     purchaseInit,
     fetchOrders
 } from './order';
+
+export {
+    authStart,
+    authSuccess,
+    authFail,
+    auth,
+    logout,
+    setAuthRedirectPath,
+    authCheckState
+} from './auth';
